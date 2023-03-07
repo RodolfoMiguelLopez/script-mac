c17cbadac3b3ccda4e7c550fd0f60a798d3c1737
Rodolfo Miguel Lopez <sitiosdepago@gmail.com>
2023-03-07 17:25:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,6 @@ echo "Hola Garajero."
 echo "Se van a instalar los programas de Garajedeideas."
 echo "Espera unos minutos."
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-if [$? -eq 0 ]
-then
-	echo "HomeBrew se ha instalado Correctamente"
-else
-	echo "Ha habido errores en la instalación de HomeBrew"
-fi
-
 brew install --cask notion
 brew install --cask google-chrome
 brew install --cask firefox
